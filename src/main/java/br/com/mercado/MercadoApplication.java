9b853b397b68560f065bf0718f4d8960ec4790bb
bruno-bergamasco <bruno.bergamasco@ibm.com>
2021-12-17 10:06:46 -0300

    criando branch developer

--- src/main/java/br/com/mercado/MercadoApplication.java
+++ src/main/java/br/com/mercado/MercadoApplication.java
@@ -33,23 +33,30 @@ public class MercadoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cliente cliente = new Cliente();
+		Cliente cliente = new Cliente(null, "Bruno Bergamasco", "322131212");
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Produto p1 = new Produto(null, "Computador");
 		Produto p2 = new Produto(null, "Notebook");
+		Estoque estoque = new Estoque();
 
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2));
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().add(cat1);
 
 		Pedido ped1 = new Pedido(null, cliente);
 		ped1.getProdutos().addAll(Arrays.asList(p1,p2));
 		p1.getPedidos().add(ped1);
 		p2.getPedidos().add(ped1);
 
+		Pedido ped2 = new Pedido(null, cliente);
+		ped2.getProdutos().addAll(Arrays.asList(p1,p2));
+		p1.getPedidos().add(ped2);
+		p2.getPedidos().add(ped2);
 
-		cliente.getPedidos().add(ped1);
+		estoque.getProdutos().add();
 
+		cliente.getPedidos().addAll(Arrays.asList(ped1,ped2));
 		clienteRepository.save(cliente);
-
 
 	}
 }
