176c92d7a2bdae528bb645e97d0db44eeae44194
bruno-bergamasco <bruno.bergamasco@ibm.com>
2021-12-31 15:57:29 -0300

    aplicando Spring Security

--- src/main/java/br/com/mercado/configurations/security/BasicSecurityConfig.java
+++ src/main/java/br/com/mercado/configurations/security/BasicSecurityConfig.java
@@ -29,7 +29,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String [] PUBLIC_MATCHERS_POST = {
             "/api/v1/pedidos/**",
-            "/api/v1/clientes/**"
+            "/api/v1/clientes/**",
+            "/api/v1/admin/login"
     };
 
     private static final String [] PUBLIC_MATCHERS_PUT = {
