db5cf907e0121480931330d0ec7dedac9597e5fc
bruno-bergamasco <bruno.bergamasco@ibm.com>
2022-01-07 01:30:53 -0300

    Swagger config

---
+++ src/main/java/br/com/mercado/configurations/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package br.com.mercado.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis( RequestHandlerSelectors.basePackage
+                        ("br.com.mercado.controller") )
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Mercadinho")
+                .description("API server side para mercado")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact("Bruno Bergamasco", "https://github.com/bergabruno", "bruno.bergamasco@ibm.com");
+    }
+}
