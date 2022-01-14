a7b8f0332bf0a63f035b8cc0e404478a710466af
bruno-bergamasco <bruno.bergamasco@ibm.com>
2022-01-14 14:54:47 -0300

    JWT

--- src/main/java/br/com/mercado/security/JWTAutheticationFilter.java
+++ src/main/java/br/com/mercado/security/JWTAutheticationFilter.java
@@ -1,50 +1,48 @@
-//package br.com.mercado.security;
-//
-//import br.com.mercado.dto.AdminNewDTO;
-//import br.com.mercado.model.entity.Admin;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.AllArgsConstructor;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//
-//@AllArgsConstructor
-//public class JWTAutheticationFilter extends UsernamePasswordAuthenticationFilter {
-//
-//    private AuthenticationManager authenticationManager;
-//    private JWTUtil jwtUtil;
-//
-//
-//
-//    @Override
-//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-//
-//        try {
-//            AdminNewDTO adminNewDTO = new ObjectMapper().readValue(request.getInputStream(), AdminNewDTO.class);
-//            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(adminNewDTO.getLogin(), adminNewDTO.getSenha(), new ArrayList<>());
-//            Authentication authentication = authenticationManager.authenticate(authToken);
-//            return authentication;
-//        }catch (IOException e){
-//            throw new RuntimeException("erro");
-//        }
-//
-//
-//    }
-//    @Override
-//    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-//        String login = ((AdminSS) authResult.getPrincipal()).getUsername();
-//        String token = jwtUtil.generateToken(login);
-//        response.addHeader("Authorization", "Bearer " + token);
-//
-//    }
-//
-//}
+package br.com.mercado.security;
+
+import br.com.mercado.dto.AdminNewDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@AllArgsConstructor
+public class JWTAutheticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+    private JWTUtil jwtUtil;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            AdminNewDTO adminNewDTO = new ObjectMapper().readValue(request.getInputStream(), AdminNewDTO.class);
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(adminNewDTO.getLogin(), adminNewDTO.getSenha(), new ArrayList<>());
+            Authentication auth = authenticationManager.authenticate(authToken);
+            return auth;
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        String login = ((AdminDetailsImpl) authResult.getPrincipal()).getUsername(); //pegando email
+        String token = jwtUtil.generateToken(login); //pegando token
+        response.addHeader("Authorization", "Bearer " + token);//cabecalho da resposta
+
+    }
+
+}
---
+++ src/main/java/br/com/mercado/security/JWTAuthorizationFilter.java
@@ -0,0 +1,53 @@
+package br.com.mercado.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+
+    private JWTUtil jwtUtil;
+
+    private AdminDetailsServiceImpl adminDetailsService;
+
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil, AdminDetailsServiceImpl adminDetailsService) {
+        super(authenticationManager);
+        this.jwtUtil = jwtUtil;
+        this.adminDetailsService = adminDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException {
+
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
+            if (auth != null) {
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(String token) {
+        if (jwtUtil.tokenValido(token)) {
+            String username = jwtUtil.getUsername(token);
+            UserDetails user = adminDetailsService.loadUserByUsername(username);
+            return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        }
+        return null;
+    }
+}
